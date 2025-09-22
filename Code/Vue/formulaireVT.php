556ad06a5848949a25d2532568058065b0e4a769
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-09-22 11:53:33 +0200

    Debut de formulaire pour le VT

---
+++ Code/Vue/formulaireVT.php
@@ -0,0 +1,12 @@
+<head>
+    <title>Ajout d'un FichierVT</title>
+</head>
+
+<h1>Ajout d'un fichier VT</h1>
+
+<form action="../Presentation/upload.php" method="post" enctype="multipart/form-data">>
+    <label for="VT">
+        FichierVT <input type="file" name="fileToUpload" id="fileToUpload"> required>
+    </label>
+    <input type="submit" value="Upload Image" name="submit" >
+</form>
