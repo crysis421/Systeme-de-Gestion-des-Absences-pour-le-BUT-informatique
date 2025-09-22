435c64333fe829d7c02dfa2bb7c86225e8cacff4
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-09-22 15:31:02 +0200

    petit ajustement et petit ajout de la page de connection

--- Code/Vue/formulaireVT.php
+++ Code/Vue/formulaireVT.php
@@ -7,7 +7,7 @@
 
 <form action="../Presentation/upload.php" method="post" enctype="multipart/form-data">>
     <label for="VT">
-        FichierVT <input type="file" name="fileToUpload" id="fileToUpload"> required>
-    </label>
+        FichierVT <input type="file" name="fileToUpload" id="fileToUpload" required>
+    </label> <br>
     <input type="submit" value="Upload Image" name="submit" >
 </form>
