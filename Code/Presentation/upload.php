ed06933e988556dd72a04b2590073970ba17864f
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-09-23 16:26:53 +0200

    commencement du transfers de donnee entre nous et VT

--- Code/Presentation/upload.php
+++ Code/Presentation/upload.php
@@ -1,23 +1,24 @@
 
-<?php // Impossible a faire en ce 22 septembre
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+<?php
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+    $row = 1;
+    if (($handle = fopen($_FILES["fileToUpload"]["tmp_name"], "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $num = count($data);
+            echo "<p> $num champs à la ligne $row: <br /></p>\n";
+            $row++;
+            $ligne = explode(";",$data[0]);
+            foreach ($ligne as $value) {
+                echo " $value ;";
+            }
+            echo "<br />";
+
+        }
+        fclose($handle);
     }
+}else{
+    echo "Aucun fichier n'a ete uploader.";
 }
 ?>
+
 
