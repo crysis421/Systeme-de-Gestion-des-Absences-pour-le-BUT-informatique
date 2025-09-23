d354bc5f9e440959b3f11191869fff5b79b0d102
christian.ekanimanga <christekanimanga@gmail.com>
2025-09-23 16:32:49 +0200

    creer du formulaire au cas l'utilisateur oublie son mot de passe

---
+++ Code/Vue/MDPoublier.php
@@ -0,0 +1,35 @@
+<?php
+require 'menuHorizontal.html'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>mot de passe oublié</title>
+</head>
+<body>
+
+<h1 style="text-align: center;">Réinitialiser votre mot de passe</h1>
+<br>
+<br>
+<br>
+<form action="" method="post">
+<label style="display: block; text-align: center; font-size: 20px" for="mail">
+    <b>entrer votre adresse mail:</b> <br>
+    <br>
+    <input type="email" id="mail" name="mail" placeholder="entrer votre adresse mail" style="width :300px" required>
+</label><br>
+    <br>
+    <div style="text-align: center">
+        <input type="submit" value="recevoir un mot de passe par mail" >
+    </div>
+</form>
+<br>
+<br>
+<br>
+<div style="padding-left: 500px">
+    <a href="connection.php"><button>retour</button></a>
+</div>
+
+</body>
+</html>
