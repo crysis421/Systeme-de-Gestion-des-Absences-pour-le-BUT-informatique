5b0170b9784d27404fd072aad07a93b46afdb6dc
Jolan Ville <jolan.ville@uphf.fr>
2025-09-24 10:29:58 +0200

    Notification faite, amélioration du viewer

--- Code/Vue/jolan/justificatif-viewer/Main.php
+++ Code/Vue/jolan/justificatif-viewer/Main.php
@@ -34,27 +34,7 @@
             </details>
         </div>
     </div>
-
-    <div class="element">
-        <div class="justificatif-viewer">
-
-            <details>
-                <summary>
-                    <a class="justificatif-texte">Justificatif</a>
-                    <img class="oeil" src="oeil.png" alt="Voir le justificatif">
-                </summary>
-                <details>
-                    <summary>
-                        <img class="justificatif-image" src="justificatif.jpg" alt="Justicatif">
-                        <img class="justificatif-close" src="close.png" alt="Fermer le justificatif">
-                    </summary>
-                    <div class="fondu-noir"></div>
-                    <img class="justificatif-image-big" src="close.png" alt="Justicatif">
-                </details>
-
-            </details>
-        </div>
-    </div>
 </div>
+
 </body>
 </html>
