f91e510dd0e2fbda6e742184cdf1a3f7a409e87d
Lefrancois <nathan.lefrancois@uphf.fr>
2025-09-24 15:02:31 +0200

    ajout de la fonction getJustificatifsAttente pour pouvoir afficher les justifiatifs en attente sur la POV du responsable

--- Code/Model/AbsenceModel.php
+++ Code/Model/AbsenceModel.php
@@ -61,6 +61,41 @@ class AbsenceModel
         return $stmt->fetchAll();
     }
 
+    public function getJustificatifsAttente() {
+        $sql = "
+        SELECT 
+            j.idJustificatif,
+            j.dateSoumission,
+            j.idMotif,
+            j.commentaire AS commentaire_justificatif,
+            j.verrouille,
+            u.idUtilisateur,
+            u.nom AS nom_etudiant,
+            u.prenom AS prenom_etudiant,
+            a.idAbsence,
+            a.statut AS statut_absence,
+            s.date AS date_seance,
+            s.heureDebut,
+            s.typeSeance,
+            c.matiere,
+            t.idTraitement,
+            t.attente,
+            t.reponse,
+            t.commentaire AS commentaire_traitement
+        FROM Justificatif j
+        JOIN Absence a ON j.idAbsence = a.idAbsence
+        JOIN Utilisateur u ON a.idUtilisateur = u.idUtilisateur
+        JOIN Seance s ON a.idSeance = s.idSeance
+        JOIN Cours c ON s.idCours = c.idCours
+        LEFT JOIN TraitementJustificatif t ON j.idJustificatif = t.idJustificatif
+        WHERE t.attente = TRUE OR t.reponse = 'enAttente'
+        ORDER BY j.dateSoumission DESC
+    ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
 
 
     public function delete($idAbsence)
