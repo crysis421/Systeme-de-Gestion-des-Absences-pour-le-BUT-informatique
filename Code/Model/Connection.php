2cc62ed6785186049cd0c4ae9173137ef46c38d6
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-09-26 14:55:55 +0200

    Liquibase

--- Code/Model/Connection.php
+++ Code/Model/Connection.php
@@ -1,1 +1,24 @@
 <?php
+$host = "iutinfo-sgbd.uphf.fr";
+$user = "iutinfo474";
+$password = "uwkXBERC";
+$dbname = "iutinfo474";
+
+
+try {
+    $conn1 = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "connected <br>";
+    $req2 = $conn1->prepare("select * from test;");
+    $req2->execute();
+    $r = $req2->fetchAll(PDO::FETCH_ASSOC);
+    foreach($r as $row){
+        foreach($row as $key => $value){
+            echo $key.": ".$value."<br>";
+        }
+        echo "<br>";
+    }
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
