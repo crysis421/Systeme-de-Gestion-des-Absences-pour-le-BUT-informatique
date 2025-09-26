5daf5d2f048bd2468647728c6ea6fef4fffb8e46
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-09-26 12:16:54 +0200

    Liquibase

--- Code/liquibase/example-changelog.sql
+++ Code/liquibase/example-changelog.sql
@@ -1,29 +1,14 @@
 --liquibase formatted sql
 
---changeset your.name:1 labels:example-label context:example-context
+--changeset Moi.name:1 labels:example-label context:example-context
 --comment: example comment
-create table person (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
+create table test (
+    id int primary key not null,
+    name varchar(50) not null
 )
---rollback DROP TABLE person;
+--rollback DROP TABLE test;
 
---changeset your.name:2 labels:example-label context:example-context
---comment: example comment
-create table company (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
---rollback DROP TABLE company;
-
---changeset other.dev:3 labels:example-label context:example-context
---comment: example comment
-alter table person add column country varchar(2)
---rollback ALTER TABLE person DROP COLUMN country;
+--changeset moi:5
+insert into test values (5,'moi');
+--rollback delete from test where id=5;
 
