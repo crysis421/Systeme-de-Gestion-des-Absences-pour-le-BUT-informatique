3c63fe63c6ecfcca976b69cdf11cdd1d211492a5
christian.ekanimanga <christekanimanga@gmail.com>
2025-09-26 14:20:38 +0200

    ajustement du fichier example-changerlog.sql

--- Code/liquibase/example-changelog.sql
+++ Code/liquibase/example-changelog.sql
@@ -2,15 +2,17 @@
 
 --changeset Moi.name:1 labels:example-label context:example-context
 --comment: example comment
-create table test (
-    id int primary key not null,
-    name varchar(50) not null
+create table test
+(
+    id   int primary key not null,
+    name varchar(50)     not null
 )
---rollback DROP TABLE test;
+    --rollback DROP TABLE test;
 
 --changeset moi:5
-insert into test values (5,'moi');
+    insert into test values (5,'moi');
 --rollback delete from test where id=5;
 
 --changeset moi:8
-select * from test
+select *
+from test
