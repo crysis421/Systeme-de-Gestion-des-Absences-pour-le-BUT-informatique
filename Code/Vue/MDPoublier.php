e1a764e60ec0b562dee5a3c45d9bf081aacfc5dc
christianekani <christekanimanga@gmail.com>
2025-10-07 21:48:22 +0100

    mise en forme des pages de connexion et du menu horizontale

--- Code/Vue/MDPoublier.php
+++ Code/Vue/MDPoublier.php
@@ -3,30 +3,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="connect.css">
+    <link rel="stylesheet" href="../CSS/connect.css">
     <title>mot de passe oublié</title>
 </head>
 <body>
-<?php require 'menuHorizontalEtu.html' ?>
-<h1 style="text-align: center;">Réinitialiser votre mot de passe</h1>
+<header>
+    <?php require 'menuHorizontalEtu.html' ?>
+</header>
+<main style="background-color: white">
+    <h1 style="text-align: center;">Réinitialiser votre mot de passe</h1>
 
-<div id="container">
-    <form id="form" action="" method="post">
-        <label style="display: block; text-align: center; font-size: 20px" for="mail">
-            <b>entrer votre adresse mail:</b> <br>
+    <div id="container">
+        <form id="form" action="" method="post">
+            <label style="display: block; text-align: center; font-size: 20px" for="mail">
+                <b>entrer votre adresse mail:</b> <br>
+                <br>
+                <input type="email" id="mail" name="mail" placeholder="entrer votre adresse mail" style="width :300px" required>
+            </label><br>
             <br>
-            <input type="email" id="mail" name="mail" placeholder="entrer votre adresse mail" style="width :300px" required>
-        </label><br>
-        <br>
+            <div >
+                <input type="submit" value="recevoir un mot de passe par mail" >
+            </div>
+        </form>
         <div >
-            <input type="submit" value="recevoir un mot de passe par mail" >
+            <a href="connection.php"><button>retour</button></a>
         </div>
-    </form>
-    <div >
-        <a href="connection.php"><button>retour</button></a>
-    </div>
 
-</div>
+    </div>
+</main>
 <footer id="footer">
     <a style="color: black" href="https://www.uphf.fr/">&copy; 2025 Université polytechnique Haut de France/ IUT de Maubeuge.</a>
 </footer>
--- Code/Vue/connection.php
+++ Code/Vue/connection.php
@@ -1,23 +1,28 @@
-<link rel="stylesheet" href="connect.css">
+<link rel="stylesheet" href="../CSS/connect.css">
 
 <body>
-<?php require_once("menuHorizontalEtu.html"); ?>
-<h1>Connection à un compte</h1>
-<div id="container">
+<header>
+    <?php require_once("menuHorizontalEtu.html"); ?>
+</header>
+<main>
+    <h1>Connection à un compte</h1>
+    <div id="container">
 
-    <form id="form" action="" method="post">
-        <label for="Email">
-            Entrer votre adresse mail : <input type="email" name="email" placeholder="adresse mail" required>
-        </label> <br>
-        <br>
-        <label for="Mot de passe">
-            Entrer votre mot de passe : <input type="password" name="motDePasse" placeholder="mot de passe" required>
-        </label> <br>
-        <a style="font-family: Arial; color: yellow; font-size: 11px;" href="MDPoublier.php">Mot de passe oublié ?</a><br>
-        <br>
-        <input   type="submit" value="Connexion">
-    </form>
-</div>
+        <form id="form" action="" method="post">
+            <label for="Email">
+                Entrer votre adresse mail : <input type="email" name="email" placeholder="adresse mail" required>
+            </label> <br>
+            <br>
+            <label for="Mot de passe">
+                Entrer votre mot de passe : <input type="password" name="motDePasse" placeholder="mot de passe" required>
+            </label> <br>
+            <a style="font-family: Arial; color: yellow; font-size: 11px;" href="MDPoublier.php">Mot de passe oublié ?</a><br>
+            <br>
+            <input   type="submit" value="Connexion">
+        </form>
+    </div>
+</main>
+
 <footer id="footer">
     <a style="color: black" href="https://www.uphf.fr/">&copy; 2025 Université polytechnique Haut de France/ IUT de Maubeuge.</a>
 </footer>
