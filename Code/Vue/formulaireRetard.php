b6e8ee355fe11bfe0a70703fa768da6010304b1b
christianekani <christekanimanga@gmail.com>
2025-10-13 14:14:27 +0100

    ajustement

--- Code/Vue/formulaireRetard.php
+++ Code/Vue/formulaireRetard.php
@@ -13,11 +13,11 @@
     <div id="titre">
         <h1>Justificatif de retard </h1>
 
-        <p id="important"><b>Important : </b> Ce formulaire doit être entièrement complété.</p>
+        <p id="important"><b>Important : </b>Ce formulaire doit être entièrement complété.</p>
     </div>
 <form action="recaptitultifJustificatifRetard.php" method="post">
     <div id="infos">
-        <h2>1) Informations de l'étudiant </h2>
+        <br>
         <label for="nom">Nom *: </label>
         <input type="text" id="nom" name="nom" placeholder="entrer votre nom" required><br>
         <br>
@@ -26,28 +26,9 @@
         <input type="text" id="prenom" name="prenom" placeholder="entrer votre prénom" required><br>
         <br>
 
-        <label for="filiere">Filière/Formation *: </label>
-        <select id="filiere" name="filiere" required>
-            <option value="BUT1INFOA" >BUT INFO 1 A</option>
-            <option value="BUT1INFOB">BUT INFO 1 B</option>
-            <option value="BUT1INFOC">BUT INFO 1 C</option>
-            <option value="BUT2INFOFIA" >BUT INFO 2 FI A</option>
-            <option value="BUT2INFOFIB">BUT INFO 2 FI B</option>
-            <option value="BUT2INFOFA">BUT INFO 2 FA</option>
-            <option value="BUT3INFOFI">BUT 3 INFO FI</option>
-            <option value="BUT3INFOFA">BUT 3 INFO FA</option>
-
-            <option value="BUT1INFOA" >BUT MP 1 </option>
-            <option value="BUT1INFOB">BUT MP 2 </option>
-            <option value="BUT1INFOC">BUT MP 3 </option>
-        </select><br>
-
     </div>
 
     <div id="inforetard">
-
-        <h2>2) Informations à propos du retard</h2>
-
 
         <label for="dateretard"> Date du jour :</label>
         <input type="date" id="dateretard" name="dateretard" required>
@@ -58,7 +39,7 @@
         <label for="cours"> Matière :</label>
         <input type="text" id="cours" name="cours" required>
 
-        <label for="motif">est arrivée en retard pour des raisons de : </label>
+        <label for="motif">Motif du retard : </label>
         <select id="motif" name="motif" required>
             <option value="Problème de santé" >Problème de santé</option>
             <option value="transport">Problème de transport</option>
@@ -76,7 +57,7 @@
         <br>
         <br>
 
-        <h2>3) Justificatif </h2>
+        <h2> Justificatif </h2>
         <label for="justificatif">Inserer un justificatif :</label><br>
         <input type="file" id="justificatif" name="justificatif" accept=".pdf,image/*" />
         <br>
@@ -84,23 +65,10 @@
         <br>
     </div>
     <div id="signature">
-        <label for="nom3">Je soussigné(e) (Nom, Prénom) :</label>
-        <input type="text" id="nom3" name="nom3" placeholder="nom et prenom " required>
-        <p> Déclarez vous sur l'honneur que les faits décrits ci‑dessus sont exacts et que les pièces justificatives fournies sont authentiques ?<br>
-            <label for="oui"><b>oui:</b></label>
+        <p> Déclarez-vous sur l'honneur que les faits décrits ci‑dessus sont exacts et que les pièces justificatives fournies sont authentiques ?<br>
+            <label for="oui"><b>Oui:</b></label>
             <input type="radio" id="oui" name="signer" required>
         </p>
-        <br>
-        <br>
-        <label for="dateSignature"><b>Fait le : </b> </label>
-        <input type="date" id="dateSignature" name="dateSignature" required>
-        <label from="heuresignature"> <b>à quelle heure ? :</b></label>
-        <input id="heuresignature" type="time" name="heuresignature"><br>
-        <br>
-        <label for="lieuSignature"><b>A : </b> </label>
-        <input type="text" id="lieuSignature" name="lieuSignature" required><br>
-        <br>
-        <br>
         <br>
         <input type="submit" value="valider">
         <br>
