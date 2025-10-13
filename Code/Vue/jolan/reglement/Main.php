ae787d48d925379a929cdecbb2dcb61f2427d67b
Jolan Ville <jolan.ville@uphf.fr>
2025-10-13 16:23:40 +0200

    Ajout de la page du réglement, et filtrage en SQL qui marche !

---
+++ Code/Vue/jolan/reglement/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Main.css">
+    <title>Tableau de bord absence</title>
+</head>
+<body>
+
+<a href="https://ent.uphf.fr/uPortal/f/Accueil/normal/render.uP"><img src="https://ent.uphf.fr/uphf/images/ent-logo.svg" alt="Logo de l'IUT"></a>
+
+
+<h1>Réglement interieur</h1>
+<a>
+    Je parle ici <b> je dis des trucs</b> très très antidepressant
+</a>
+
+</body>
+</html>
