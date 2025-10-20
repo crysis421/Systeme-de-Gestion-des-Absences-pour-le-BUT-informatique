8ff76343379747f6da7d2041e1950b49b4809efd
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-10-20 16:10:00 +0200

    Tableau bord etu fini

--- Code/Model/AbsenceModel.php
+++ Code/Model/AbsenceModel.php
@@ -230,11 +230,21 @@ class AbsenceModel
     }
 
     //Cette fonction nous permet de recupérer les infos d'une absence d'un etudiant a un jour précis , utiliser pour le tableau de bord de l'etudiant
-    public function getAbsenceDunJour($date,$idEtudiant,$mois) {
-        $stmt = $this->conn->prepare("SELECT statut,estRetard,heureDebut,prof,duree,enseignement,typeSeance,salle,controle FROM absence JOIN Seance using(idSeance) WHERE idEtudiant = :idEtudiant and extract('Days' from Seance.date) = :d and extract('Months' from Seance.date) = :m");
+    public function getAbsenceDunJour($date,$idEtudiant,$mois,$year) {
+        $stmt = $this->conn->prepare("SELECT statut,estRetard,heureDebut,prof,duree,enseignement,typeSeance,salle,controle FROM absence JOIN Seance using(idSeance) WHERE idEtudiant = :idEtudiant and extract('Days' from Seance.date) = :d and extract('Months' from Seance.date) = :m and extract('Years' from Seance.date) = :year");
         $stmt->bindParam(":idEtudiant", $idEtudiant);
         $stmt->bindParam(":d", $date);
         $stmt->bindParam(":m", $mois);
+        $stmt->bindParam(":year", $year);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getAbsenceDunMois($idEtudiant,$mois,$year) {
+        $stmt = $this->conn->prepare("SELECT statut,extract('Days' from Seance.date) FROM absence JOIN Seance using(idSeance) where extract('Months' from Seance.date) = :m and extract('Years' from Seance.date) = :year and idEtudiant = :idEtudiant");
+        $stmt->bindParam(":idEtudiant", $idEtudiant);
+        $stmt->bindParam(":m", $mois);
+        $stmt->bindParam(":year", $year);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
