bc57e0e93007b5f625207d1dfa208a26092f74e5
miquel.sanz <Miquel.Sanz@uphf.fr>
2025-11-03 11:44:58 +0100

    Reglage Menu Horizontal Etu

---
+++ Code/Vue/CompteEtu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../CSS/formulaire.css" />
+    <title>Title</title>
+</head>
+<header>
+    <?php require 'menuHorizontalEtu.html'; ?>
+</header>
+<body>
+
+<h1>Compte</h1>
+<button name="Deconnexion">Déconnexion</button>
+
+</body>
+</html>
