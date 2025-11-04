424090438ecbe1af53635e5c2f1b02eba5977d86
christian.ekanimanga <christekanimanga@gmail.com>
2025-11-04 09:22:27 +0100

    mise en forme de la partie "compte" de l'étudiant

--- Code/Vue/CompteEtu.php
+++ Code/Vue/CompteEtu.php
@@ -3,35 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../CSS/compte.css" />
-    <title>Title</title>
+    <title>Mon compte</title>
 </head>
+
+<body>
 <header>
     <?php require 'menuHorizontalEtu.html'; ?>
 </header>
-<body>
+
 <main>
     <h1>Bonjour Christian ! 👋</h1>
-    <div id="profil">
-        <img src="/Image/profil-removebg-preview.png" alt="profil">
-        <h2>Profil</h2>
-        <b>Nom : </b><br>
-        <b>Prénom : </b><br>
-        <b>Numéro d'étudiant : </b><br>
-        <b>Email : </b><br>
-        <b>Formation : </b>
-        <b>Groupe : </b><br>
-        <button>Modifier profil</button>
+
+    <!-- Section du haut : Profil à gauche / Données à droite -->
+    <div id="haut">
+        <div id="profil">
+            <img src="/Image/profil-removebg-preview.png" alt="Photo de profil">
+            <h2>Profil</h2>
+            <p><b>Nom :</b> Dupont</p>
+            <p><b>Prénom :</b> Christian</p>
+            <p><b>Numéro d'étudiant :</b> 123456</p>
+            <p><b>Email :</b> christian@example.com</p>
+            <p><b>Formation :</b> BUT Informatique</p>
+            <p><b>Groupe :</b> A2</p>
+            <button>Modifier le profil</button>
+        </div>
+
+        <div id="donnees">
+            <h2>Mes absences</h2>
+            <p><b>Nombre d'absences :</b> 5</p>
+            <p><b>Justifiées :</b> 3</p>
+            <p><b>Non justifiées :</b> 2</p>
+        </div>
     </div>
 
-    <div id="donne">
-        <b>Nombre d'absences : </b><br>
-        <b>Nombre d'absences justifié : </b><br>
-        <b>Nombre d'absences Non justifié : </b><br>
+    <!-- Section du bas, centrée -->
+    <div id="bas">
+        <div id="graphe">
+            <b>Ajouter des diagrammes ici</b>
+        </div>
     </div>
-
 </main>
+
 <footer id="footer">
-    <a style="color: black" href="https://www.uphf.fr/">&copy; 2025 Université polytechnique Haut de France/ IUT de Maubeuge.</a>
+    <a href="https://www.uphf.fr/" style="color: black; text-decoration: none;">
+        &copy; 2025 Université Polytechnique Haut-de-France / IUT de Maubeuge
+    </a>
 </footer>
 </body>
 </html>
