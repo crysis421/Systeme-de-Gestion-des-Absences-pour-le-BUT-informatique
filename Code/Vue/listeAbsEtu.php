b8cc713c3348fd7d0dbff6646f9f34b072cc8104
Stievenard <Kilian.Stievenard2@uphf.fr>
2025-11-06 08:54:12 +0100

    tableauDeBordEtu a son prime

--- Code/Vue/listeAbsEtu.php
+++ Code/Vue/listeAbsEtu.php
@@ -2,39 +2,57 @@
 //Ce fichier est là pour le Tableau De Bord de l'étudiant
 echo '<link rel="stylesheet" href="tableauDeBordResponsable.css">';
 
-if(empty($result)){
+if (empty($result)) {
     echo "<p>Aucune absence n’a été enregistrée à votre nom pour la journée </p>";
-}else{
+} else {
     ?>
     <main><br><br></main>
     <form action="formulaireAbsence.php" method="get">
-        <input type="submit" value="Justifier les absences du <?php echo $_SESSION['jour']."/".date_format($_SESSION['date'],"m/y");?>" id="jour" name="date">
+        <input type="submit"
+               value="Justifier les absences du <?php echo $_SESSION['jour'] . "/" . date_format($_SESSION['date'], "m/y"); ?>"
+               id="jour" name="date">
     </form>
 
     <?php
     foreach ($result as $absence):
         ?>
-        <div class="element" <?php if ($absence['statut']=="valide"){echo 'id=valide';}else if($absence['statut']=="report"){echo 'id=enAttente';}else{echo 'id=refus';} ?>>
+        <div class="element" <?php if ($absence['statut'] == "valide") {
+            echo 'id=valide';
+        } else if ($absence['statut'] == "report") {
+            echo 'id=enAttente';
+        } else {
+            echo 'id=refus';
+        } ?>>
             <details>
                 <summary class="top-layer">
-                    <img src="../Image/profil_default.png" alt="avatar" class="image-utilisateur" height="24">
-                    <a class="nom"><b><?= htmlspecialchars($absence['prof'])?></a><br>
-
-                    <div class="description-element">
-                        <small><?= htmlspecialchars($absence['enseignement']) ?></small>
-                        <br><small><?= htmlspecialchars(date($_SESSION["jour"].'/ '.$_SESSION['mois'])) ?> à <?= htmlspecialchars($absence['heuredebut']) ?></small>
-                    </div>
-                    <?php if ($absence['controle']){
-                        ?>
-                        <small id="intero">⚠ Interrogation</small>
-                        <?php
-                    }?>
+                    <ol id="maListe">
+                        <img src="../Image/profil_default.png" alt="avatar"
+                             class="image-utilisateur" height="24">
+                        <a class="nom"><b><?= htmlspecialchars($absence['prof']) ?></a><br>
+                        <li class="elementDeListe">
+                            <div <?php echo 'class=' . $absence['statut']; ?> id="liste"></div>
+                        </li>
+                        <li class="elementDeListe">
+                            <?php if ($absence['controle']) {
+                                ?>
+                                <small id="intero">⚠ Interrogation</small>
+                                <?php
+                            } ?>
+                            <div class="description-element">
+                                <small><?= htmlspecialchars($absence['enseignement']) ?></small>
+                                <br><small><?= htmlspecialchars(date($_SESSION["jour"] . '/ ' . $_SESSION['mois'])) ?>
+                                    à <?= htmlspecialchars($absence['heuredebut']) ?></small>
+                            </div>
+                        </li>
+                    </ol>
                     <div class="ligne" id="maLigne"></div>
                 </summary>
             </details>
         </div>
-    <?php endforeach;}?>
+    <?php endforeach;
+} ?>
 <main><br></main>
 <footer id="footer">
-    <a style="color: black" href="https://www.uphf.fr/">&copy; 2025 Université polytechnique Haut de France/ IUT de Maubeuge.</a>
+    <a style="color: black" href="https://www.uphf.fr/">&copy; 2025 Université polytechnique Haut de France/ IUT de
+        Maubeuge.</a>
 </footer>
