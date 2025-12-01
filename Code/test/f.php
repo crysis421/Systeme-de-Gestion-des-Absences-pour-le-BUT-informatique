0aa6371517718805825c280703fb47ed66f5b337
christianekani <christekanimanga@gmail.com>
2025-12-01 16:16:10 +0100

    phpmailer

--- Code/test/f.php
+++ Code/test/f.php
@@ -1,3 +1,26 @@
+<?php
+require_once '../test/mail.php';
+use test\mail;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mail = $_POST["email"];
+    $contenu = "<h3>Confirmation de Dépôt de votre justificatif</h3>
+                <p>Votre Justificatif a bien été envoyé</p>";
+
+    $mailer = new mail();
+
+    $result = $mailer->envoyer($mail,'Confirmation de depot de justificatif',$contenu);
+    if ($result) {
+        echo "<p>Votre mail a été envoyé avec succès !</p>";
+    } else {
+        echo "<p>Échec de l'envoi du mail.</p>";
+    }
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,7 +29,7 @@
 </head>
 <body>
 <h2>Contactez-nous</h2>
-<form action="send.php" method="POST">
+<form action="" method="POST">
     <label>Votre nom :</label><br>
     <input type="text" name="name" required><br><br>
 
@@ -16,7 +39,7 @@
     <label>Message :</label><br>
     <textarea name="message" required></textarea><br><br>
 
-    <button type="submit">Envoyer</button>
+    <button type="submit">Envoyer';</button>
 </form>
 </body>
 </html>
