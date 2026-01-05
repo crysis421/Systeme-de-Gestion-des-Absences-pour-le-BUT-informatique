5f9d473da76fd7b7bdb7e7bc2701e2f019352753
Stievenard <Kilian.Stievenard2@uphf.fr>
2026-01-05 15:04:04 +0100

    PHPUnit que je vais faire sur mon pc

---
+++ Code/test/AbsenceModelTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace test;
+
+use AbsenceModel;
+use PHPUnit\Framework\TestCase;
+
+class AbsenceModelTest extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testCreate()
+    {
+
+
+    }
+
+}
