e92babdcac7e352066ddb87fa4eb32aadb6a7ced
Jolan Ville <jolan.ville@uphf.fr>
2026-03-13 11:01:00 +0100

    Filtre ajax, juste un problème de CSS

---
+++ Code/Vue/liste_absences_partielle.php
@@ -0,0 +1,92 @@
+<?php
+if (empty($groupes)) {
+    echo "<p style='padding:20px;'>Aucun résultat trouvé.</p>";
+} else {
+    foreach ($groupes as $justif):
+        $id = $justif['id'];
+        $commentaire = $justif['commentaire'];
+        $absences = $justif['absences'];
+        ?>
+        <div class="element">
+            <details>
+                <summary class="top-layer">
+                    <img src="/Image/profil_default.png" alt="avatar" class="image-utilisateur" height="24">
+                    <a class="nom"><b><?= htmlspecialchars($justif['nom']) ?> <?= htmlspecialchars($justif['prenom']) ?></b></a><br>
+                    <div class="description-element">
+                        <small><?= htmlspecialchars($justif['description']) ?></small><br>
+                        <small>Soumis le <?= htmlspecialchars($justif['datesoumission']) ?></small>
+                    </div>
+                    <div class="ligne"></div>
+                </summary>
+
+                <div class="details">
+                    <div class="justificatif-viewer">
+                        <details>
+                            <summary>
+                                <a class="justificatif-texte">Justificatif</a>
+                                <img class="oeil" src="/Image/oeil.png" alt="Voir le justificatif">
+                            </summary>
+                            <!-- Code du zoom image -->
+                            <input type="checkbox" id="zoom<?= $id ?>" name="zoom" style="display: none;">
+                            <label for="zoom<?= $id ?>" class="zoom-button"></label>
+                            <label for="zoom<?= $id ?>" class="justificatif-close">
+                                <img src="/Image/close.png" alt="Fermer le justificatif">
+                            </label>
+                            <br><a><b>Commentaire :</b><br> <?php echo $commentaire ?></a> <br>
+                            <div class="fondu-noir"></div>
+                            <img class="justificatif-image-big" src="/Image/justificatif.jpg" alt="Justificatif">
+                        </details>
+                    </div>
+
+                    <form method="post">
+                        <input type="hidden" name="IDElement" value="<?= $id ?>" >
+
+                        <?php foreach ($absences as $abs):
+                            $matiere = $abs['matiere'];
+                            if (preg_match('#\((.*?)\)#', $matiere, $match)) {
+                                $matiere = explode("-", $match[1])[1] ?? $matiere;
+                            }
+                            ?>
+                            <input type="checkbox" name="checkboxAbsence[]" value="<?= $abs['id'] ?>" id="checkboxAbsence_<?= $abs['id'] ?>" checked>
+                            <label for="checkboxAbsence_<?= $abs['id'] ?>">
+                                <?= htmlspecialchars($abs['date'])?> <?= htmlspecialchars(substr($abs['heure'],0,5))?> <?= htmlspecialchars($matiere)?>
+                            </label> <br>
+                        <?php endforeach; ?>
+
+                        <a class="decision-finale">Décision finale</a>
+                        <!-- Boutons radios de décision -->
+                        <input type="radio" id="toggle1_<?= $id ?>" name="toggle" value="accepte" style="display: none;">
+                        <label for="toggle1_<?= $id ?>" class="label-accepter"></label>
+
+                        <input type="radio" id="toggle2_<?= $id ?>" name="toggle" value="refuse" style="display: none;">
+                        <label for="toggle2_<?= $id ?>" class="label-refuser"></label>
+
+                        <input type="radio" id="toggle3_<?= $id ?>" name="toggle" value="demande" style="display: none;">
+                        <label for="toggle3_<?= $id ?>" class="label-demander"></label>
+
+                        <br><br>
+                        <!-- Zones de texte conditionnelles -->
+                        <div class="texte-accepter">
+                            <select name="motifs">
+                                <option value="transport">Transport</option>
+                                <option value="malade">Malade</option>
+                            </select>
+                            <input class='bouton-envoye' type="submit" name="bouton4" value="Envoyer">
+                        </div>
+
+                        <div class="texte-refuser">
+                            Motif du refus : <textarea name="motif_refus"></textarea>
+                            <input class='bouton-envoye' type="submit" name="bouton4" value="Envoyer">
+                        </div>
+
+                        <div class="texte-demander">
+                            Motif de la demande : <textarea name="motif_demande"></textarea>
+                            <input class='bouton-envoye' type="submit" name="bouton4" value="Envoyer">
+                        </div>
+                        <div class="ligne2"></div>
+                    </form>
+                </div>
+            </details>
+        </div>
+    <?php endforeach;
+} ?>
--- Code/Vue/tableauDeBordResponsable.php
+++ Code/Vue/tableauDeBordResponsable.php
@@ -11,6 +11,7 @@ require_once "../Presentation/responTB.php"
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../CSS/tableauDeBordResponsable.css">
+    <script src="../ajax/ajaxFiltre.js"></script>
     <title>Tableau de bord absence</title>
 </head>
 <body>
@@ -42,36 +43,29 @@ EOL;
     </summary>
 
     <div class="filtrage">
-        <form method="post" id="formFiltre">
-
+        <form id="formFiltre">
             <div class="dateFiltre">
-                <?php
-                $dateDebut = '2020-01-01';
-                $dateFin = date("Y-m-d");
-                ?>
-
                 <h3>Date de début</h3>
-                <input type="date" id="startDate" name="dateDebut" value="<?= $dateDebut ?>">
+                <input type="date" id="startDate" name="dateDebut" value="2020-01-01">
                 <h3>Date de fin</h3>
-                <input type="date" id="endDate" name="dateFin" value="<?= $dateFin ?>">
+                <input type="date" id="endDate" name="dateFin" value="<?= date("Y-m-d") ?>">
             </div>
 
             <div class="matiereFiltre">
                 <h3>Nom de la matière</h3>
-                <input type="text" id="inputMatiere" name="Matière" value="">
+                <input type="text" id="inputMatiere" name="Matière">
             </div>
 
             <div class="eleveFiltre">
                 <h3>Prénom</h3>
-                <input type="text" id="inputPrenom" name="PrenomInput" value="">
+                <input type="text" id="inputPrenom" name="PrenomInput">
                 <h3>Nom</h3>
-                <input type="text" id="inputNom" name="NomInput" value="">
+                <input type="text" id="inputNom" name="NomInput">
             </div>
 
-
-            <input class='bouton-filtrage' type="submit" name="boutonFiltre" value="Filtrer">
-
+            <input class='bouton-filtrage' type="submit" value="Filtrer">
         </form>
+
 
         <br>
     </div>
@@ -79,117 +73,8 @@ EOL;
 
 
 <!-- Liste des absences ici ! -->
-<div class="liste-absence">
-    <?php foreach ($groupes as $justif):
-        $id = $justif['id'];
-        $commentaire = $justif['commentaire'];
-        $absences = $justif['absences'];
-        ?>
-        <div class="element">
-            <details>
-                <summary class="top-layer">
-                    <img src="/Image/profil_default.png" alt="avatar" class="image-utilisateur" height="24">
-                    <a class="nom"><b><?= htmlspecialchars($justif['nom']) ?> <?= htmlspecialchars($justif['prenom']) ?></b></a><br>
-                    <div class="description-element">
-                        <small><?= htmlspecialchars($justif['description']) ?></small><br>
-                        <small>Soumis le <?= htmlspecialchars($justif['datesoumission']) ?></small>
-                    </div>
-
-                    <div class="ligne"></div>
-                </summary>
-
-                <div class="details">
-                    <div class="justificatif-viewer">
-                        <details>
-                            <summary>
-                                <a class="justificatif-texte">Justificatif</a>
-                                <img class="oeil" src="/Image/oeil.png" alt="Voir le justificatif">
-                            </summary>
-
-                            <input type="checkbox" id="zoom<?= $id ?>" name="zoom" style="display: none;">
-                            <label for="zoom<?= $id ?>" class="zoom-button"></label>
-
-                            <label for="zoom<?= $id ?>" class="justificatif-close">
-                                <img src="/Image/close.png" alt="Fermer le justificatif">
-                            </label>
-
-                            <br><a><b>Commentaire :</b><br> <?php echo $commentaire ?></a> <br>
-
-                            <div class="fondu-noir"></div>
-                            <img class="justificatif-image-big" src="/Image/justificatif.jpg" alt="Justificatif">
-                        </details>
-                    </div>
-                    <form method="post">
-
-                        <input type="hidden" name="IDElement" value="<?= $justif['id'] ?>" >
-
-                        <?php foreach ($absences as $abs):
-
-                            $matiere = $abs['matiere'];
-                            preg_match('#\((.*?)\)#', $matiere, $match);
-                            $matiere = $match[1];
-                            $matiere = explode("-", $matiere)[1];
-
-                            $date = $abs['date'];
-                            $heure = $abs['heure'];
-                            $idAbsence = $abs['id'];
-                            $statusAbsence = $abs['status'];
-                            if($statusAbsence != 'report') continue;
-                            ?>
-                            <input type="checkbox" name="checkboxAbsence[]" value="<?= $abs['id'] ?>" id="checkboxAbsence_<?= $abs['id'] ?>" checked>
-                            <label for="checkboxAbsence_<?= $abs['id'] ?>"><?= htmlspecialchars($abs['date'])?> <?= htmlspecialchars(rtrim(substr($abs['heure'],0,5),')'))?> <?= htmlspecialchars($matiere)?></label> <br>
-                        <?php endforeach; ?>
-
-                        <a class="decision-finale">Décision finale</a>
-
-                        <input type="radio" id="toggle1_<?= $id ?>" name="toggle" value="accepte" style="display: none;">
-                        <label for="toggle1_<?= $id ?>" class="label-accepter"></label>
-
-                        <input type="radio" id="toggle2_<?= $id ?>" name="toggle" value="refuse" style="display: none;">
-                        <label for="toggle2_<?= $id ?>" class="label-refuser"></label>
-
-                        <input type="radio" id="toggle3_<?= $id ?>" name="toggle" value="demande" style="display: none;">
-                        <label for="toggle3_<?= $id ?>" class="label-demander"></label>
-
-                        <input type="hidden" id="IDElement_<?= $id ?>" value="<?= $id ?>" name="IDElement">
-
-                        <br><br>
-
-                        <div class="texte-accepter">
-                            <select name="motifs" id="motif-absence-<?= $id ?>">
-                                <option value="">--Choisissez une option--</option>
-                                <option value="transport">Transport</option>
-                                <option value="malade">Malade</option>
-                                <option value="etc">...</option>
-                            </select>
-                            <br>
-                            <input class='bouton-envoye' type="submit" name="bouton4" value="Envoyer">
-                            <br><br>
-                        </div>
-
-                        <div class="texte-refuser">
-                            Motif du refus : <br><br>
-                            <textarea name="motif_refus" rows="4" cols="50"></textarea>
-                            <br><br>
-                            <input class='bouton-envoye' type="submit" name="bouton4" value="Envoyer">
-                            <br><br>
-                        </div>
-
-                        <div class="texte-demander">
-                            Motif de la demande : <br><br>
-                            <textarea name="motif_demande" rows="4" cols="50"></textarea>
-                            <br><br>
-                            <input class='bouton-envoye' type="submit" name="bouton4" value="Envoyer">
-                            <br><br>
-                        </div>
-                        <div class="ligne2"></div>
-                    </form>
-
-                </div>
-
-            </details>
-        </div>
-    <?php endforeach; ?>
+<div class="liste-absence" id="conteneurAbsences">
+    <?php include "liste_absences_partielle.php";?>
 </div>
 <br><br>
 
